3c9de7a308fba65bbd6f2b192d7b7968a7088b5e
jokerdnr91 <102039968+jokerdnr91@users.noreply.github.com>
2025-04-15 03:06:08 +0200

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+mport React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1>To-do Liste Luxe</h1>
+      <p>Bienvenue dans votre application premium de tâches partagées.</p>
+    </div>
+  )
+}
