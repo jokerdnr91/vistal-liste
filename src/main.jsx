b1612494e5a14469e39acc619f1ddb514049cb58
jokerdnr91 <102039968+jokerdnr91@users.noreply.github.com>
2025-04-15 04:46:58 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-// Import Firebase (ça initialise la connexion à Firebase)
-import './firebaseConfig';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
