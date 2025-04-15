2d6078aeadacf0f41f8e364c39a9ccbc68b74816
jokerdnr91 <102039968+jokerdnr91@users.noreply.github.com>
2025-04-15 03:57:27 +0200

    Create firebaseConfig.js
    FIREBASSE

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCcSD1M87Dau5fSdFTi4lWbZg_GQLaZwcc",
+  authDomain: "vistaliste.firebaseapp.com",
+  projectId: "vistaliste",
+  storageBucket: "vistaliste.firebasestorage.app",
+  messagingSenderId: "442466725851",
+  appId: "1:442466725851:web:e183605cdbddfdeb4101fe"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
