a75a7c564e539ec8cc4c32876e77abaeabfecf54
jokerdnr91 <102039968+jokerdnr91@users.noreply.github.com>
2025-04-15 02:29:13 +0200

    Create app.jsx
    APP.JSX

---
+++ app.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { db, auth } from './firebase'
+
+function App() {
+  return (
+    <div>
+      <h1>To-Do Liste Luxe</h1>
+      <p>Bienvenue dans ton app de luxe.</p>
+    </div>
+  )
+}
+
+export default App
