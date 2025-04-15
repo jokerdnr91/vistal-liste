50a3b15e5a4d6b7ff6148c135f5e27696dda76d1
jokerdnr91 <102039968+jokerdnr91@users.noreply.github.com>
2025-04-15 04:19:55 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
 
 // Import Firebase (ça initialise la connexion à Firebase)
 import './firebaseConfig';
